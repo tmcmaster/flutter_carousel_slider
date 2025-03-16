9a0da1aedbf44f497ee7679f29964047f1a1bf16
Tim McMaster <tim@mcmaster.id.au>
2025-03-16 13:27:31 +1100

    material.dart' hide CarouselController

--- lib/carousel_controller.dart
+++ lib/carousel_controller.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide CarouselController;
 
 import 'carousel_options.dart';
 import 'carousel_state.dart';
@@ -38,8 +38,7 @@ class CarouselSliderControllerImpl implements CarouselSliderController {
     }
   }
 
-  void _setModeController() =>
-      _state!.changeMode(CarouselPageChangedReason.controller);
+  void _setModeController() => _state!.changeMode(CarouselPageChangedReason.controller);
 
   @override
   bool get ready => _state != null;
@@ -51,9 +50,7 @@ class CarouselSliderControllerImpl implements CarouselSliderController {
   ///
   /// The animation lasts for the given duration and follows the given curve.
   /// The returned [Future] resolves when the animation completes.
-  Future<void> nextPage(
-      {Duration? duration = const Duration(milliseconds: 300),
-      Curve? curve = Curves.linear}) async {
+  Future<void> nextPage({Duration? duration = const Duration(milliseconds: 300), Curve? curve = Curves.linear}) async {
     final bool isNeedResetTimer = _state!.options.pauseAutoPlayOnManualNavigate;
     if (isNeedResetTimer) {
       _state!.onResetTimer();
@@ -70,15 +67,13 @@ class CarouselSliderControllerImpl implements CarouselSliderController {
   /// The animation lasts for the given duration and follows the given curve.
   /// The returned [Future] resolves when the animation completes.
   Future<void> previousPage(
-      {Duration? duration = const Duration(milliseconds: 300),
-      Curve? curve = Curves.linear}) async {
+      {Duration? duration = const Duration(milliseconds: 300), Curve? curve = Curves.linear}) async {
     final bool isNeedResetTimer = _state!.options.pauseAutoPlayOnManualNavigate;
     if (isNeedResetTimer) {
       _state!.onResetTimer();
     }
     _setModeController();
-    await _state!.pageController!
-        .previousPage(duration: duration!, curve: curve!);
+    await _state!.pageController!.previousPage(duration: duration!, curve: curve!);
     if (isNeedResetTimer) {
       _state!.onResumeTimer();
     }
@@ -89,8 +84,8 @@ class CarouselSliderControllerImpl implements CarouselSliderController {
   /// Jumps the page position from its current value to the given value,
   /// without animation, and without checking if the new value is in range.
   void jumpToPage(int page) {
-    final index = getRealIndex(_state!.pageController!.page!.toInt(),
-        _state!.realPage - _state!.initialPage, _state!.itemCount);
+    final index =
+        getRealIndex(_state!.pageController!.page!.toInt(), _state!.realPage - _state!.initialPage, _state!.itemCount);
 
     _setModeController();
     final int pageToJump = _state!.pageController!.page!.toInt() + page - index;
@@ -102,30 +97,24 @@ class CarouselSliderControllerImpl implements CarouselSliderController {
   /// The animation lasts for the given duration and follows the given curve.
   /// The returned [Future] resolves when the animation completes.
   Future<void> animateToPage(int page,
-      {Duration? duration = const Duration(milliseconds: 300),
-      Curve? curve = Curves.linear}) async {
+      {Duration? duration = const Duration(milliseconds: 300), Curve? curve = Curves.linear}) async {
     final bool isNeedResetTimer = _state!.options.pauseAutoPlayOnManualNavigate;
     if (isNeedResetTimer) {
       _state!.onResetTimer();
     }
-    final index = getRealIndex(_state!.pageController!.page!.toInt(),
-        _state!.realPage - _state!.initialPage, _state!.itemCount);
+    final index =
+        getRealIndex(_state!.pageController!.page!.toInt(), _state!.realPage - _state!.initialPage, _state!.itemCount);
     int smallestMovement = page - index;
-    if (_state!.options.enableInfiniteScroll &&
-        _state!.itemCount != null &&
-        _state!.options.animateToClosest) {
+    if (_state!.options.enableInfiniteScroll && _state!.itemCount != null && _state!.options.animateToClosest) {
       if ((page - index).abs() > (page + _state!.itemCount! - index).abs()) {
         smallestMovement = page + _state!.itemCount! - index;
-      } else if ((page - index).abs() >
-          (page - _state!.itemCount! - index).abs()) {
+      } else if ((page - index).abs() > (page - _state!.itemCount! - index).abs()) {
         smallestMovement = page - _state!.itemCount! - index;
       }
     }
     _setModeController();
-    await _state!.pageController!.animateToPage(
-        _state!.pageController!.page!.toInt() + smallestMovement,
-        duration: duration!,
-        curve: curve!);
+    await _state!.pageController!
+        .animateToPage(_state!.pageController!.page!.toInt() + smallestMovement, duration: duration!, curve: curve!);
     if (isNeedResetTimer) {
       _state!.onResumeTimer();
     }
